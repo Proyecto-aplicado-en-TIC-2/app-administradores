8429659e440c50f04a61c6033b1816dc82e1d282
johamja <johamfranco318@gmail.com>
2024-10-23 19:28:40 -0500

    feat: Vista de informes

---
+++ src/app/components/table-history-closed-reports/table-history-closed-reports.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import {FilledButtonComponent} from "../filled-button/filled-button.component";
+
+@Component({
+  selector: 'app-table-history-closed-reports',
+  standalone: true,
+  imports: [
+    FilledButtonComponent
+  ],
+  template: `
+    <table>
+      <thead>
+      <tr class="roboto-regular">
+        <td><h3>Fecha de incidente</h3></td>
+        <td><h3>Nombre completo</h3></td>
+        <td><h3>Aph asignado</h3></td>
+        <td><h3>Bloque</h3></td>
+        <td><h3>Descripción</h3></td>
+      </tr>
+      </thead>
+      <tbody class="roboto-regular">
+      <tr>
+        <td class="text">Jaider Joham Morales Franco</td>
+        <td class="text">Bloque 11</td>
+        <td class="text">Me realice un corte</td>
+        <td class="text">Maria perez</td>
+        <td class="text">Maria perez</td>
+        <td>
+          <app-filled-button texto="Informe"/>
+        </td>
+      </tr>      <tr>
+        <td class="text">Jaider Joham Morales Franco</td>
+        <td class="text">Bloque 11</td>
+        <td class="text">Me realice un corte</td>
+        <td class="text">Maria perez</td>
+        <td class="text">Maria perez</td>
+        <td>
+          <app-filled-button texto="Informe"/>
+        </td>
+      </tr>      <tr>
+        <td class="text">Jaider Joham Morales Franco</td>
+        <td class="text">Bloque 11</td>
+        <td class="text">Me realice un corte</td>
+        <td class="text">Maria perez</td>
+        <td class="text">Maria perez</td>
+        <td>
+          <app-filled-button texto="Informe"/>
+        </td>
+      </tr>
+
+
+      </tbody>
+    </table>
+  `,
+  styles: `
+    table {
+      width: 100%;
+      border-spacing: 0 20px;
+    }
+
+    thead {
+      h3 {
+        margin: 0;
+        padding: 0 20px;
+      }
+    }
+
+    tbody {
+      border-spacing: 10px 0;
+
+      td {
+        padding: 0 20px;
+      }
+
+      .text {
+        border-right: 1px solid var(--md-sys-color-on-surface);
+      }
+    }
+  `
+})
+export class TableHistoryClosedReportsComponent {
+
+}
--- src/app/main/report/report.component.ts
+++ src/app/main/report/report.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {FilledButtonComponent} from "../../components/filled-button/filled-button.component";
+import {TableOpenReportsComponent} from "../../components/table-open-reports/table-open-reports.component";
+import {
+  TableHistoryClosedReportsComponent
+} from "../../components/table-history-closed-reports/table-history-closed-reports.component";
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [],
+  imports: [
+    FilledButtonComponent,
+    TableOpenReportsComponent,
+    TableHistoryClosedReportsComponent
+  ],
   templateUrl: './report.component.html',
   styleUrl: './report.component.css'
 })
