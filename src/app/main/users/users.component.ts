b0468ed7344e1b54ca97cbbaf5d53a2a0003577a
johamja <johamfranco318@gmail.com>
2024-10-23 20:47:16 -0500

    feat: Vista de usuarios aph, comunidad upb y brigadistas

--- src/app/main/users/users.component.ts
+++ src/app/main/users/users.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {FilledButtonComponent} from "../../components/filled-button/filled-button.component";
+import {TableAphUsersComponent} from "../../components/table-users/table-aph-users.component";
+import {TableAphBrigadiersComponent} from "../../components/table-users/table-aph-brigadiers.component";
+import {TableAphUpbCommunityComponent} from "../../components/table-users/table-aph-upb-community.component";
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [
+    FilledButtonComponent,
+    TableAphUsersComponent,
+    TableAphBrigadiersComponent,
+    TableAphUpbCommunityComponent
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
